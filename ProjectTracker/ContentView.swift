1aa079659b5a6b751b98ebf643f1362c83ae39be
sklohrm <sklohrm@gmail.com>
2022-08-25 22:59:26 -0500

    Initial Commit

---
+++ ProjectTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 8/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectTracker/ProjectTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrackerApp.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
