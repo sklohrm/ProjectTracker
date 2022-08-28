b921c329e12a67e751e24d05fc8de6742dadfc2b
sklohrm <sklohrm@gmail.com>
2022-08-28 14:25:15 -0500

    Added EditItemView

---
+++ ProjectTracker/EditItemView.swift
@@ -0,0 +1,20 @@
+//
+//  EditItemView.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 8/28/22.
+//
+
+import SwiftUI
+
+struct EditItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditItemView()
+    }
+}
---
+++ ProjectTracker/ItemRowView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemRowView.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 8/28/22.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView()
+    }
+}
