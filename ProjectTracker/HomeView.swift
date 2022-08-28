0b357a2b1b48d750bef3a4ce0a21b8d392582668
sklohrm <sklohrm@gmail.com>
2022-08-28 10:46:29 -0500

    Added CoreData Model, Preview data, and basic app navigation.

---
+++ ProjectTracker/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 8/28/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
