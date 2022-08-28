93f832d7d0331f07866df4685ab5536ca719a5a0
sklohrm <sklohrm@gmail.com>
2022-08-28 15:54:39 -0500

    Added project headers and color assets

---
+++ ProjectTracker/ProjectHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectHeaderView.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 8/28/22.
+//
+
+import SwiftUI
+
+struct ProjectHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProjectHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectHeaderView()
+    }
+}
