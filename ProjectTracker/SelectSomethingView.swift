b3840cc9747ece996d72c9daf28551c21d4c414f
Spencer <sklohrm@gmail.com>
2022-08-30 11:57:48 -0500

    Updated ProjectView to account for larger screens.

---
+++ ProjectTracker/SelectSomethingView.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSomethingView.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 8/30/22.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text("Please select something from the menu to begin.")
+            .italic()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
