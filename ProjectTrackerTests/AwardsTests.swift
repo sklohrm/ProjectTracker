5455e13ab7966b0c103b782535639c3085efb0cf
sklohrm <sklohrm@gmail.com>
2022-09-05 13:31:01 -0500

    Added Tests

--- ProjectTrackerTests/AwardsTests.swift
+++ ProjectTrackerTests/AwardsTests.swift
@@ -5,31 +5,69 @@
 //  Created by Spencer Lohrmann on 9/5/22.
 //
 
+import CoreData
 import XCTest
+@testable import ProjectTracker
 
 class AwardsTests: BaseTestCase {
+    let awards = Award.allAwards
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAwardIDMatchesName() {
+        for award in awards {
+            XCTAssertEqual(award.id, award.name, "Award ID should always match its name.")
         }
     }
 
+    func testNewUserHasNoAwards() {
+        for award in awards {
+            XCTAssertFalse(dataController.hasEarned(award: award), "New user should have no earned awards.")
+        }
+    }
+
+    func testAddingItems() {
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+
+        for (count, value) in values.enumerated() {
+            var items = [Item]()
+
+            for _ in 0..<value {
+                let item = Item(context: managedObjectContext)
+                items.append(item)
+            }
+
+            let matches = awards.filter { award in
+                award.criterion == "items" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1, "Adding \(value) items should unlock \(count + 1) awards.")
+
+            for item in items {
+                dataController.delete(item)
+            }
+        }
+    }
+
+    func testCompletingItems() {
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+
+        for (count, value) in values.enumerated() {
+            var items = [Item]()
+
+            for _ in 0..<value {
+                let item = Item(context: managedObjectContext)
+                item.completed = true
+                items.append(item)
+            }
+
+            let matches = awards.filter { award in
+                award.criterion == "complete" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1, "Completing \(value) items should unlock \(count + 1) awards.")
+
+            for item in items {
+                dataController.delete(item)
+            }
+        }
+    }
 }
--- ProjectTrackerTests/ProjectTrackerTests.swift
+++ ProjectTrackerTests/ProjectTrackerTests.swift
@@ -5,31 +5,17 @@
 //  Created by Spencer Lohrmann on 9/5/22.
 //
 
+import CoreData
 import XCTest
+@testable import ProjectTracker
 
-class ProjectTrackerTests: XCTestCase {
+class BaseTestCase: XCTestCase {
+    var dataController: DataController!
+    var managedObjectContext: NSManagedObjectContext!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        dataController = DataController(inMemory: true)
+        managedObjectContext = dataController.container.viewContext
     }
 
 }
