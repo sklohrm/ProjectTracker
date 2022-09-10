b2b45dfb82d81f81811ebe8a86c33c349fe31080
sklohrm <sklohrm@gmail.com>
2022-09-10 15:05:24 -0500

    Created HomeViewModel

---
+++ ProjectTracker/Activities/Projects/ItemRowViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  ItemRowViewModel.swift
+//  ProjectTracker
+//
+//  Created by Spencer Lohrmann on 9/10/22.
+//
+
+import Foundation
+
+extension ItemRowView {
+    class ViewModel: ObservableObject {
+        let project: Project
+        let item: Item
+
+        init(project: Project, item: Item) {
+            self.project = project
+            self.item = item
+        }
+
+        var title: String {
+            item.itemTitle
+        }
+
+        var icon: String {
+            if item.completed {
+                return "checkmark.circle"
+            } else if item.priority == 3 {
+                return "exclamationmark.triangle"
+            } else {
+                return "checkmark.circle"
+            }
+        }
+
+        var color: String? {
+            if item.completed {
+                return project.projectColor
+            } else if item.priority == 3 {
+                return project.projectColor
+            } else {
+                return nil
+            }
+        }
+
+        var label: String {
+            if item.completed {
+                return "\(item.itemTitle), completed."
+            } else if item.priority == 3 {
+                return "\(item.itemTitle), high priority."
+            } else {
+                return item.itemTitle
+            }
+        }
+    }
+}
